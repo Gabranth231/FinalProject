2403377430da2c5e0cca4bd1f94e9b4aafd78efe
Gabranth231 <boycie3164@gmail.com>
2021-12-17 22:33:15 +0000

    Committing done since last push, was putting it off since i ran into a brick wall with the mix couloums part and i didn't want to put up broken code

--- Program/src/main.java
+++ Program/src/main.java
@@ -1,7 +1,47 @@
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class main {
 
     public static void main(String[] args) {
 	    Encryption128 obj = new Encryption128();
-        obj.Encrypt();
+        char[] encryptedText;
+        char[] temp = new char[16];
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter text to encrypt: ");
+        String myText = sc.nextLine();
+
+        int originalLenght = myText.length();
+        int paddedTextLen = originalLenght;
+
+        if(paddedTextLen % 16 != 0){                    //pads the message to make sure the message is a multiple of 16
+            paddedTextLen = (paddedTextLen / 16 + 1) * 16;
+        }
+        char[] paddedText = new char[paddedTextLen];
+        for(int i = 0;i<paddedTextLen;i++){
+            if(i>=originalLenght){
+                paddedText[i] = 0;
+            }
+            else{
+                paddedText[i] = myText.charAt(i);
+            }
+        }
+        int i = 0;
+        encryptedText = new char[paddedTextLen];
+        while (i < paddedTextLen){
+            for(int a = 0;a<16;a++){
+                temp[a] = paddedText[a];
+            }
+            char[] t = obj.encrypt(temp);
+            for(int a = 0;a<16;a++){
+                encryptedText[i+a] = t[a];
+            }
+            i+=16;
+        }
+
+        for(char a: encryptedText){
+            System.out.printf(String.format("%02x ",(int) a));
+        }
     }
 }
